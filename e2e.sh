354de61203c8d0f2f76428ead83c9841128311fe
Russ Watson <russ@bakerstreet.industries>
2017-11-27 21:51:08 -0600

    Unit tests blocked by the class logger not being written in TS. Sad Face.

--- e2e.sh
+++ e2e.sh
@@ -4,11 +4,16 @@ serverless deploy
 
 # Parse out the http endpoint
 endpoint="$(serverless info | grep POST - | sed 's/POST - //')"
-echo "${endpoint}"
 
-# Replace the module tests.spec.ts endpoint token
-# Sadly, I could not find a way to do a simple string replacement using SED because of the slashes in the endpoint. Dumping out the responsibility to gulp
+# Replace the module tests.e2e.ts endpoint token
+# Sadly, my SED skills are lacking and I could not find a way to do a simple string replacement because of the slashes in the endpoint. Dumping out the responsibility to gulp.
 gulp --endpoint ${endpoint}
 
+# Deploy function updates
+serverless deploy --function module-post
+serverless deploy --function module-del
+serverless deploy --function module-get
+serverless deploy --function module-pu
+
 # Run the integration tests
-npm run e2e
+nyc mocha **/*.e2e.ts
--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,7 @@ var gulp = require('gulp');
 var replace = require('gulp-string-replace');
 
 gulp.task('default', function () {
-    console.log('What?', process.argv, process.argv[2]);
-    gulp.src(["./module/tests.spec.ts"])
+    gulp.src(["./module/tests.e2e.ts"])
         .pipe(replace(/\[\[ENDPOINT\]\]/g, process.argv[3] + '/'))
         .pipe(gulp.dest('./module/'))
 });
