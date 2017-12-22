8a21b2699e5548bd7a3a8d5099fda4829500dc53
Russ Watson <russ@bakerstreet.industries>
2017-12-22 08:51:04 -0600

    Now only parsing and updating the e2e endpoint when the token is present.

--- e2e.sh
+++ e2e.sh
@@ -2,18 +2,15 @@
 
 serverless deploy
 
-# Parse out the http endpoint
-endpoint="$(serverless info | grep POST - | sed 's/POST - //')"
+if grep [[ENDPOINT]] ./module/tests/e2e.ts
+then
+    # Parse out the http endpoint
+    endpoint="$(serverless info | grep POST - | sed 's/POST - //')"
 
-# Replace the module tests.e2e.ts endpoint token
-# Sadly, my SED skills are lacking and I could not find a way to do a simple string replacement because of the slashes in the endpoint. Dumping out the responsibility to gulp.
-gulp --endpoint ${endpoint}
-
-# Deploy function updates
-serverless deploy --function module-post
-serverless deploy --function module-del
-serverless deploy --function module-get
-serverless deploy --function module-put
+    # Replace the module tests.e2e.ts endpoint token
+    # Sadly, my SED skills are lacking and I could not find a way to do a simple string replacement because of the slashes in the endpoint. Dumping out the responsibility to gulp.
+    gulp --endpoint ${endpoint}
+fi
 
 # Run the integration tests
 nyc mocha **/e2e.ts
